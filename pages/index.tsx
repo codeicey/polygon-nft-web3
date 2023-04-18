d11d00298bcc0f07c1bd65fd99d3cabd3686c3b8
Madhukar Gaida <86058993+madhukar123456@users.noreply.github.com>
2023-04-18 18:23:15 +0545

    change to polygon testnet mumbai

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   }, [])
   async function loadNFTs() {
     /* create a generic provider and query for unsold market items */
-    const provider = new ethers.providers.JsonRpcProvider()
+    const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.matic.today")
     const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
     const data = await contract.fetchMarketItems()
 
